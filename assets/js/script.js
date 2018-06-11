6471616dd470f84f3a6f46ea43ba78e750c02608
Nitish Khagwal <nitishkmrk@gmail.com>
2018-06-11 11:51:30 +0530

    Added PH Dialog

--- assets/js/script.js
+++ assets/js/script.js
@@ -50,4 +50,62 @@ document.addEventListener("DOMContentLoaded", function () {
             });
         }
     }
+
+    // <div class="fade-overlay">
+    //     <div class="dialog">
+    //         <h2>Upvote on <span class="label">ProductHunt</span></h2>
+    //         <p>You can really appreciate Vivid.JS - Open Source SVG Icons Library on ProductHunt to show your support.</p>
+    //         <div class="buttons">
+    //             <a class="dg-upvote" target="_blank" rel="noopener" href="https://www.producthunt.com/posts/vivid-js-svg-icons-javascript-library">Upvote</a>
+    //             <a class="dg-close" href="javascript:void(0)">I don't care</a>
+    //         </div>
+            
+    //     </div>
+    // </div>
+    var vBody = document.body;
+    var vfadeOverlay = document.querySelector(".fade-overlay");
+    var vDialog = document.querySelector(".dialog");
+    var vUpvote = document.querySelector(".dg-upvote");
+    var vClose = document.querySelector(".dg-close");
+    var dialogPreview = localStorage.getItem("dialog");
+    console.log(dialogPreview);
+    
+    var hideDialog = function(){
+        vfadeOverlay.classList.add("hide-overlay");
+        setTimeout(function(){
+            if(vBody.classList.contains("overflow-blocked")){
+                vBody.classList.remove("overflow-blocked");
+            }
+            if(vfadeOverlay.classList.contains("fade-overlay-show")){
+                vfadeOverlay.classList.remove("fade-overlay-show");
+            }
+        },500);
+    }
+
+    var showDialog = function(){
+       if(vfadeOverlay != undefined && vfadeOverlay != null){
+            vBody.classList.add("overflow-blocked");
+            vfadeOverlay.classList.add("fade-overlay-show");
+            vUpvote.addEventListener("click", function(){
+                hideDialog();
+                if(typeof(Storage) !== "undefined"){
+                    localStorage.setItem("dialog","hidden");
+                }
+            });
+            vClose.addEventListener("click", function(){
+                hideDialog();
+                if(typeof(Storage) !== "undefined"){
+                    localStorage.setItem("dialog","hidden");
+                }
+            });
+       }
+    };
+
+    if(typeof(Storage) !== "undefined"){
+        if(dialogPreview == undefined || dialogPreview == null || dialogPreview !== "hidden"){
+            console.log(dialogPreview);
+                setTimeout(showDialog, 15000);
+        }
+    }
+
 });
